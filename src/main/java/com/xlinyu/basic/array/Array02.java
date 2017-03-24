37990a342d76c6a87774d35afde816d9dc0c1fe5
ncc0706 <ncc0706@gmail.com>
2017-03-24 23:42:33 +0800

    数组一旦初始化后，长度不能再改变

---
+++ src/main/java/com/xlinyu/basic/array/Array02.java
@@ -0,0 +1,51 @@
+package com.xlinyu.basic.array;
+
+public class Array02 {
+
+	public static void main(String[] args) {
+		
+		// 1. 数组就是相同数据类型的有序集合
+		// 2. 数组也是对象.数组元素相当于对象的成员变量
+		// 3. 数组长度是确定的,不可变的.
+		
+		// 使用数组前必须初始化
+		
+		/**
+		 * 初始化的方式:
+		 * 1. 静态初始化
+		 * 2. 动态初始化
+		 */
+		
+		// 静态初始化
+		String[] books = new String[]{
+			"Java In Action",
+			"Hadoop In Action",
+			"Maven In Action"
+		};
+		
+		// 静态初始化(简化方式)
+		String[] names = {
+			"唐玄奘",
+			"孙悟空",
+			"猪八戒",
+			"沙悟净"
+		};
+		
+		// 动态初始化
+		String[] strArr = new String[5];
+		
+		System.out.println(books.length);
+		System.out.println(names.length);
+		System.out.println(strArr.length);
+		
+		books = names;
+		strArr = names;
+		
+		System.out.println("-----指向names的引用后----");
+		System.out.println(names.length);
+		System.out.println(strArr.length);
+		
+		
+	}
+	
+}
