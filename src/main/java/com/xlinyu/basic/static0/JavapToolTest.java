6adfe0943eef6896e4ee05e16928b1a978e90245
ncc0706 <ncc0706@gmail.com>
2017-03-25 11:23:41 +0800

    javap分析

---
+++ src/main/java/com/xlinyu/basic/static0/JavapToolTest.java
@@ -0,0 +1,13 @@
+package com.xlinyu.basic.static0;
+
+public class JavapToolTest {
+
+	int count = 20;
+	{
+		count = 10;
+	}
+	
+	public JavapToolTest(){
+		System.out.println(count);
+	}	
+}
--- src/main/java/com/xlinyu/basic/static0/README.md
+++ src/main/java/com/xlinyu/basic/static0/README.md
@@ -9,3 +9,6 @@ static 修饰的成员变量是类变量, 非static修饰的成员变量是实�
 + 初始化块
 + 定义变量时指定初始化值
 
+javac java源文件 `javap -c java class文件`分析{初始化块} 指定初始化值
+
+[](/resources/01.png)
