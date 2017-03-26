06deb9ee5e497183f96e5c603eddbd6a66885acc
ncc0706 <ncc0706@gmail.com>
2017-03-26 21:30:37 +0800

    可变参

---
+++ src/main/java/com/xlinyu/basic/args/Args.java
@@ -0,0 +1,25 @@
+package com.xlinyu.basic.args;
+
+public class Args {
+
+	public static void main(String[] args) {
+//		sum(new int[]{1, 2, 3, 4, 5});
+		
+		fun("C", "C++", "Java", "Python");
+	}
+	
+	public static void sum(int... nums){
+		int sum = 0;
+		for(int i=0;i<nums.length;i++){
+			sum += nums[i];
+		}
+		System.out.println("sum: " + sum);
+	}
+	
+	public static void fun(String... strs){
+		for(int i=0;i<strs.length;i++){
+			System.out.println(strs[i]);
+		}
+	}
+	
+}
