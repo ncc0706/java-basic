e4a4b1aad14faf04fa0293758eb0c0e3d12a41d9
ncc0706 <ncc0706@gmail.com>
2017-03-30 20:14:27 +0800

    main Thread

---
+++ src/main/java/com/xlinyu/basic/thread00/t01/Run00.java
@@ -0,0 +1,9 @@
+package com.xlinyu.basic.thread00.t01;
+
+public class Run00 {
+
+	public static void main(String[] args) {
+		// 主线程 main
+		System.out.println(Thread.currentThread().getName());
+	}
+}
