82d0a4592a597581a6cea3c2cb1548cce019227a
ncc0706 <ncc0706@gmail.com>
2017-03-30 20:08:46 +0800

    println()内部是同步的

---
+++ src/main/java/com/xlinyu/basic/thread00/t01/Run06.java
@@ -0,0 +1,31 @@
+package com.xlinyu.basic.thread00.t01;
+
+public class Run06 {
+
+public static void main(String[] args) {
+		
+		//此时变量就共享了,但是存在线程安全问题
+		MyThread mt = new MyThread();
+		Thread t1 = new Thread(mt, "A");
+		Thread t2 = new Thread(mt, "B");
+		Thread t3 = new Thread(mt, "C");
+		Thread t4 = new Thread(mt, "D");
+		Thread t5 = new Thread(mt, "E");
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+	}
+	
+	static class MyThread extends Thread {
+		
+		private int count = 5;
+		
+		@Override
+		public synchronized void run() {
+			System.out.println("threadName=" + Thread.currentThread().getName() + ", count=" + (count--) );
+		}
+	}
+}
