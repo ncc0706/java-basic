bc7fcf73ca6015355e038fb7ef669bb2f01041c3
ncc0706 <ncc0706@gmail.com>
2017-04-06 22:36:56 +0800

    定时的执行任务

---
+++ src/main/java/com/xlinyu/basic/thread/executor/ScheduledThreadPool01.java
@@ -0,0 +1,25 @@
+package com.xlinyu.basic.thread.executor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledThreadPool01 {
+
+	public static void main(String[] args) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		// 创建一个定长线程池，支持定时及周期性任务执行
+		ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(15);
+		scheduledThreadPool.scheduleAtFixedRate(new Runnable() {
+			public void run() {
+				System.out.println(sdf.format(new Date()) + " -> delay 3 seconds");
+			}
+		}, 3, 3, TimeUnit.SECONDS);
+		System.out.println(sdf.format(new Date()));
+	}
+
+}
