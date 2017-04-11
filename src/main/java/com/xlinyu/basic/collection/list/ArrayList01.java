253997783102e86f4c31b1515eaaad515406016d
ncc0706 <ncc0706@gmail.com>
2017-04-11 08:35:12 +0800

    update

--- src/main/java/com/xlinyu/basic/collection/list/ArrayList01.java
+++ src/main/java/com/xlinyu/basic/collection/list/ArrayList01.java
@@ -7,7 +7,7 @@ public class ArrayList01 {
 
 	public static void main(String[] args) {
 		// 有序可以重复
-		List<Object> data = new ArrayList<>();
+		List<Object> data = new ArrayList<Object>();
 		
 		data.add(123);
 		data.add(123);
